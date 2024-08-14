45af7c177ecfa59d7b7e92d3add7b1920fbfba0a
Kingstaafk <83715216+Kingstaafk@users.noreply.github.com>
2024-08-14 12:37:07 +0530

    hi

--- js/main.js
+++ js/main.js
@@ -61,3 +61,11 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+//popup//
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
